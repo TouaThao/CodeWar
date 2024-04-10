c0d1479cdb3d1fd665abded84fb69c205f69a097
TouaThao <touathao09@gmail.com>
2024-04-09 23:47:48 -0500

    Rank up move files

---
+++ 8 kyu/TotalAmountOfPoints.js
@@ -0,0 +1,81 @@
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings.
+// Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+  let total = 0;
+  for (let i = 0; i < games.length; i++) {
+    let scores = games[i].split(":");
+    let x = parseInt(scores[0]);
+    let y = parseInt(scores[1]);
+
+    if (x > y) {
+      total += 3;
+    } else if (x == y) {
+      total += 1;
+    }
+  }
+  return total;
+}
+
+console.log(
+  (points([
+    "1:0",
+    "2:0",
+    "3:0",
+    "4:0",
+    "2:1",
+    "3:1",
+    "4:1",
+    "3:2",
+    "4:2",
+    "4:3",
+  ]),
+  30)
+);
+// console.log(
+//   (points([
+//     "1:1",
+//     "2:2",
+//     "3:3",
+//     "4:4",
+//     "2:2",
+//     "3:3",
+//     "4:4",
+//     "3:3",
+//     "4:4",
+//     "4:4",
+//   ]),
+//   10)
+// );
+// console.log(
+//   (points([
+//     "0:1",
+//     "0:2",
+//     "0:3",
+//     "0:4",
+//     "1:2",
+//     "1:3",
+//     "1:4",
+//     "2:3",
+//     "2:4",
+//     "3:4",
+//   ]),
+//   0)
+// );
--- 8 kyu/keepHydrate.js
+++ 8 kyu/keepHydrate.js
@@ -11,3 +11,12 @@
 // time = 6.7---> litres = 3
 
 // time = 11.8--> litres = 5
+
+function litres(time) {
+  let total = Math.floor(time * 0.5);
+  return total;
+}
+
+console.log(litres(2), 1);
+console.log(litres(1.4), 0);
+console.log(litres(12.3), 6);
