bb191ff7f0469b8ab44dabbc1bbdf71a9c9b4330
TouaThao <touathao09@gmail.com>
2024-04-10 22:57:04 -0500

    More code

---
+++ 7 Kyu/ConvertANumberToAString.js
@@ -0,0 +1,6 @@
+function numberToString(num) {
+  let stringifyNums = num.toString();
+  return stringifyNums;
+}
+
+console.log(numberToString(123));
---
+++ 7 Kyu/SimpleMultiplication.js
@@ -0,0 +1,22 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+  const x = number % 2 ? number * 8 : number * 9;
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
+
+console.log(
+  simpleMultiplication(2),
+  16,
+  "Should return given argument times eight..."
+);
+console.log(
+  simpleMultiplication(1),
+  9,
+  "Should return given argument times nine..."
+);
+console.log(
+  simpleMultiplication(8),
+  64,
+  "Should return given argument times eight..."
+);
