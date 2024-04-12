13f29cc94dbd351c83a978ae83c7bf1715418b94
TouaThao <touathao09@gmail.com>
2024-04-11 23:07:49 -0500

    add code

---
+++ 7 Kyu/HowGoodAreYou.js
@@ -0,0 +1,23 @@
+// There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+
+// Note:
+// Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+function betterThanAverage(classPoints, yourPoints) {
+  let classTotal = classPoints.reduce((a, c) => a + c);
+  if (yourPoints >= classTotal) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(betterThanAverage([2, 3], 5));
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75));
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9));
--- 7 Kyu/SimpleMultiplication.js
+++ 7 Kyu/SimpleMultiplication.js
@@ -1,7 +1,6 @@
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 function simpleMultiplication(number) {
-  const x = number % 2 ? number * 8 : number * 9;
   return number % 2 === 0 ? number * 8 : number * 9;
 }
 
