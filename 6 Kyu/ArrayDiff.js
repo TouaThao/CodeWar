a00e49bf8b7e5ae2d8921f31109dcd464767cae5
TouaThao <touathao09@gmail.com>
2024-04-30 22:27:00 -0500

    Did more code

--- 6 Kyu/ArrayDiff.js
+++ 6 Kyu/ArrayDiff.js
@@ -7,4 +7,17 @@
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
-function arrayDiff(a, b) {}
+function arrayDiff(a, b) {
+  let array = [];
+  for (let i = 0; i < a.length; i++) {
+    if (!b.includes(a[i])) {
+      array.push(a[i]);
+    }
+  }
+  return array;
+}
+
+console.log(arrayDiff([1, 2], [1]), [2], "a was [1,2], b was [1]");
+console.log(arrayDiff([1, 2, 2], [1]), [2, 2], "a was [1,2,2], b was [1]");
+console.log(arrayDiff([1, 2, 2], [2]), [1], "a was [1,2,2], b was [2]");
+console.log(arrayDiff([1, 2, 2], []), [1, 2, 2], "a was [1,2,2], b was []");
--- 8 kyu/ReadMe.md
+++
@@ -1,1 +0,0 @@
-A repository filled with codewars challenges
